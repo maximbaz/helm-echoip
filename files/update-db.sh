5f4c037c9a6b0d28c815e05c3be724866f17b5b0
Maxim Baz <git@maximbaz.com>
2018-08-05 03:01:02 +0200

    Improve update-db.sh script

--- files/update-db.sh
+++ files/update-db.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 
+dir="/db"
+cd "$dir" || exit 2
+
+if [ "$1" = "init" ] && [ -f GeoLite2-City.mmdb ] && [ -f GeoLite2-Country.mmdb ]; then
+    exit 0
+fi
+
+procdir="$(mktemp -d -p "$dir")"
+cd "$procdir" || exit 3
+
 download() {
     url="http://geolite.maxmind.com/download/geoip/database"
     target="$1"
     archive="$1.tar.gz"
 
-    mkdir tmp
-    cd tmp
+    tmpdir="$(mktemp -d -p "$procdir")"
+    cd "$tmpdir" || exit 4
 
     wget "$url/$archive"
     tar -xvzf "$archive"
-    mv $target*/*.mmdb ../
+    mv "$target"*/*.mmdb "$procdir"
 
-    cd ..
-    rm -r tmp
+    cd "$procdir" || exit 5
+    rm -rf "$tmpdir"
     md5sum "$target.mmdb" > "$target.mmdb.md5"
 }
 
-cd /db
-
-if [ "$1" = "init" -a -f GeoLite2-City.mmdb -a -f GeoLite2-Country.mmdb ]; then
-    exit 0
-fi
-
 download "GeoLite2-City"
 download "GeoLite2-Country"
+mv ./* "$dir"
+
+cd "$dir" || exit 6
+rm -rf "$procdir"
